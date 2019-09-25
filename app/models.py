65a9e3314706d614c9d56a43e20b1ac1e86d4d53
Alex <slicer256@gmail.com>
2019-09-25 23:42:16 +0300

    added first models

---
+++ app/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from datetime import datetime
+
+class Court(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    map_lat = models.FloatField(default=0)
+    map_lng = models.FloatField(default=0)
+
+class Player(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255)
+    courts = models.ManyToManyField(Court)
+    is_looking = models.IntegerField(default=0)
