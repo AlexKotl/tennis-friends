89436c8ae61caf217ad437432b8a287d08d84ec2
Alex <slicer256@gmail.com>
2019-09-26 00:32:12 +0300

    str method for models

--- app/models.py
+++ app/models.py
@@ -7,6 +7,9 @@ class Court(models.Model):
     map_lat = models.FloatField(default=0)
     map_lng = models.FloatField(default=0)
 
+    def __str__(self):
+        return f"{self.name}"
+
 class Player(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
@@ -14,3 +17,6 @@ class Player(models.Model):
     phone = models.CharField(max_length=255)
     courts = models.ManyToManyField(Court)
     is_looking = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.email})"
