c1697f7cb239edce5ea7639dcd8947d36448c159
Alex <slicer256@gmail.com>
2019-09-27 14:46:44 +0300

    models updated

--- app/models.py
+++ app/models.py
@@ -6,6 +6,9 @@ class Court(models.Model):
     address = models.CharField(max_length=255)
     map_lat = models.FloatField(default=0)
     map_lng = models.FloatField(default=0)
+    flag = models.IntegerField(default=1)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.name}"
@@ -17,6 +20,9 @@ class Player(models.Model):
     phone = models.CharField(max_length=255)
     courts = models.ManyToManyField(Court)
     is_looking = models.IntegerField(default=0)
+    flag = models.IntegerField(default=1)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} ({self.email})"
