741b415415f8d00258e07cd5cc7e65c6cb5acf46
Alex <slicer256@gmail.com>
2019-10-02 22:55:44 +0300

    player experience added

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth import get_user_model
+from datetime import datetime
 from .models import Player, Court
 
 class PlayerCreationForm(UserCreationForm):
@@ -16,10 +17,14 @@ class PlayerCreationForm(UserCreationForm):
         help_text="Корты на которых вы играете"
     )
     rank = forms.ChoiceField(
+        label="Уровень игры",
         choices=[(x/10, x/10) for x in range(10, 75, 5)],
-        help_text="<a href='#' data-toggle='modal' data-target='#rankModal'>Как определить свой уровень?</a>"
+        help_text="<a href='#' data-toggle='modal' data-target='#rankModal'>Как определить свой уровень?</a>",
     )
-    player_since = forms.CharField(label='Когда вы начали играть?', max_length=100)
+    player_since = forms.ChoiceField(
+        label='Игровой опыт',
+        choices=[(0, '-')] + [(x, f'с {x} года') for x in range(datetime.today().year, datetime.today().year - 10, -1)],
+    )
 
     class Meta:
         model = Player
