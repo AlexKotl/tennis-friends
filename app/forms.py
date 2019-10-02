e70116e18945d6d04a944afe8ad174ebb47db928
Alex <slicer256@gmail.com>
2019-10-02 22:44:12 +0300

    misc registration form improvements

--- app/forms.py
+++ app/forms.py
@@ -4,21 +4,26 @@ from django.contrib.auth import get_user_model
 from .models import Player, Court
 
 class PlayerCreationForm(UserCreationForm):
-    first_name = forms.CharField(label='Имя', max_length=100, help_text="Имя")
+    first_name = forms.CharField(label='Имя', max_length=100, help_text="Ваше полное имя")
     email = forms.EmailField(label='Email', max_length=100)
-    phone = forms.CharField(label='Телефон', max_length=100)
-    courts = forms.ModelChoiceField(queryset=Court.objects.all(),
+    phone = forms.CharField(label='Телефон', max_length=100, help_text="Телефон будет скрыт от других пользователей")
+    courts = forms.ModelChoiceField(
+        queryset=Court.objects.all(),
         widget=forms.CheckboxSelectMultiple,
         required=False,
         empty_label=None,
         label="Корты",
-        help_text="Корты на которых вы играете")
-    rank = forms.CharField(label='Ваш уровень', max_length=100)
+        help_text="Корты на которых вы играете"
+    )
+    rank = forms.ChoiceField(
+        choices=[(x/10, x/10) for x in range(10, 75, 5)],
+        help_text="<a href='#' data-toggle='modal' data-target='#rankModal'>Как определить свой уровень?</a>"
+    )
     player_since = forms.CharField(label='Когда вы начали играть?', max_length=100)
 
     class Meta:
         model = Player
-        fields = ('username', "first_name", 'email')
+        fields = ("first_name", 'email')
 
 class PlayerChangeForm(UserChangeForm):
     class Meta:
