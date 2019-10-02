3b789dba8cad86cadc6acb40160bcb35bf92656d
Alex <slicer256@gmail.com>
2019-10-02 19:35:09 +0300

    couts field on user register form

--- app/forms.py
+++ app/forms.py
@@ -1,13 +1,18 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth import get_user_model
-from .models import Player
+from .models import Player, Court
 
 class PlayerCreationForm(UserCreationForm):
     first_name = forms.CharField(label='Имя', max_length=100, help_text="Имя")
     email = forms.EmailField(label='Email', max_length=100)
     phone = forms.CharField(label='Телефон', max_length=100)
-    courts = forms.CharField(label='Корты на которых вы играете', max_length=100)
+    courts = forms.ModelChoiceField(queryset=Court.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False,
+        empty_label=None,
+        label="Корты",
+        help_text="Корты на которых вы играете")
     rank = forms.CharField(label='Ваш уровень', max_length=100)
     player_since = forms.CharField(label='Когда вы начали играть?', max_length=100)
 
