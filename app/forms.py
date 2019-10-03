a053f4766817c413a63ceeb70ab1fe4793d9f7f4
Alex <slicer256@gmail.com>
2019-10-03 15:58:20 +0300

    save more firlds in registration form

--- app/forms.py
+++ app/forms.py
@@ -8,27 +8,26 @@ class PlayerCreationForm(UserCreationForm):
     first_name = forms.CharField(label='Имя', max_length=100, help_text="Ваше полное имя")
     email = forms.EmailField(label='Email', max_length=100)
     phone = forms.CharField(label='Телефон', max_length=100, help_text="Телефон будет скрыт от других пользователей")
-    courts = forms.ModelChoiceField(
+    courts = forms.ModelMultipleChoiceField(
         queryset=Court.objects.all(),
         widget=forms.CheckboxSelectMultiple,
         required=False,
-        empty_label=None,
         label="Корты",
         help_text="Корты на которых вы играете"
     )
-    # rank = forms.ChoiceField(
-    #     label="Уровень игры",
-    #     choices=[(x/10, x/10) for x in range(10, 75, 5)],
-    #     help_text="<a href='#' data-toggle='modal' data-target='#rankModal'>Как определить свой уровень?</a>",
-    # )
-    # player_since = forms.ChoiceField(
-    #     label='Игровой опыт',
-    #     choices=[(0, '-')] + [(x, f'с {x} года') for x in range(datetime.today().year, datetime.today().year - 10, -1)],
-    # )
+    rank = forms.ChoiceField(
+        label="Уровень игры",
+        choices=[(x/10, x/10) for x in range(10, 75, 5)],
+        help_text="<a href='#' data-toggle='modal' data-target='#rankModal'>Как определить свой уровень?</a>",
+    )
+    player_since = forms.ChoiceField(
+        label='Игровой опыт',
+        choices=[(0, '-')] + [(x, f'с {x} года') for x in range(datetime.today().year, datetime.today().year - 10, -1)],
+    )
 
     class Meta:
         model = Player
-        fields = ('username', 'email', 'first_name',)
+        fields = ('username', 'email', 'first_name', 'phone', 'courts', 'rank', 'player_since')
 
     def clean_username(self):
         print("cleaning username ")
@@ -44,4 +43,4 @@ class PlayerCreationForm(UserCreationForm):
 class PlayerChangeForm(UserChangeForm):
     class Meta:
         model = Player
-        fields = ('username', 'email')
+        fields = ('username', 'email',)
