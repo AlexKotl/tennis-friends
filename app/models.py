08f6f493a7a5937cfe8e6b902bb4984dc0138e73
Alex <slicer256@gmail.com>
2019-10-05 21:37:30 +0300

    courtes model change

--- app/models.py
+++ app/models.py
@@ -6,6 +6,9 @@ from datetime import datetime
 class Court(models.Model):
     name = models.CharField(max_length=255)
     address = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255,default="")
+    url = models.CharField(max_length=255,default="")
+    image = models.CharField(max_length=255,default="")
     map_lat = models.FloatField(default=0)
     map_lng = models.FloatField(default=0)
     flag = models.IntegerField(default=1)
@@ -16,8 +19,6 @@ class Court(models.Model):
         return f"{self.name}"
 
 class Player(AbstractUser):
-    # first_name = models.CharField(max_length=100)
-    # last_name = models.CharField(max_length=100)
     username = models.CharField(max_length=180, unique=False)
     email = models.CharField(max_length=180, unique=True)
     phone = models.CharField(max_length=180)
