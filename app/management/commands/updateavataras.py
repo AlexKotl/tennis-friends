1c6221712e2d7ff22addd86bff30378f288fc868
Alex <slicer256@gmail.com>
2019-10-22 00:26:34 +0300

    update all avataras management tool

--- app/management/commands/updateavataras.py
+++ app/management/commands/updateavataras.py
@@ -4,8 +4,18 @@ from app.models import Player
 class Command(BaseCommand):
     help = 'Updates all empty user avataras using API'
 
+    def add_arguments(self, parser):
+        parser.add_argument('--update-all',
+            help='Force update all avatars that failed.',
+            nargs='?',
+            default=False)
+
     def handle(self, *args, **options):
-        players = Player.objects.filter(image_url="")[:10]
+        if options['update_all'] == False:
+            players = Player.objects.filter(image_url="")[:10]
+        else:
+            players = Player.objects.filter(image_url="-")[:100]
+
         for player in players:
             avatara = player.get_avatar(player.email)
             player.image_url = avatara
