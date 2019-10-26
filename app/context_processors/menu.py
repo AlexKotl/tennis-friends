6bea5d4b3921f4b074078e24dee3a79595342b13
Alex <slicer256@gmail.com>
2019-10-27 00:22:33 +0300

    count unread messages count in menu

--- app/context_processors/menu.py
+++ app/context_processors/menu.py
@@ -1,6 +1,13 @@
 from django.template.context_processors import request
+from app.models import Player, Message
 
 def menu(request):
+    if request.user.id == None:
+        return {'menu_friends_count': ''}
+
+    user = Player.objects.get(pk=request.user.id)
+    messages = Message.objects.filter(recipient=user, is_read=False)
+
     return {
-        "menu_friends_count": 0
+        "menu_friends_count": messages.count
     }
