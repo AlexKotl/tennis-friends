56d4c7746f8ec65f2941d7ef684934efc1f8f83b
Alex <slicer256@gmail.com>
2019-10-27 22:56:38 +0200

    proper form inputs for player image field

--- app/models.py
+++ app/models.py
@@ -42,13 +42,14 @@ class Player(AbstractUser):
         return f"{self.first_name} {self.last_name} ({self.email})"
 
     def save(self, *args, **kwargs):
-        try:
-            super().save() # first save image
-            im = Image.open(self.image.path)
-            im.thumbnail((1000, 1000))
-            im.save(self.image.path, "JPEG")
-        except IOError:
-            print("cannot create thumbnail for", self.image)
+        if self.image:
+            try:
+                super().save() # first save image
+                im = Image.open(self.image.path)
+                im.thumbnail((1000, 1000))
+                im.save(self.image.path, "JPEG")
+            except IOError:
+                print("cannot create thumbnail for", self.image)
 
         super(Player, self).save(*args, **kwargs)
 
