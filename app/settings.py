f65a6f2a37268dc03a6c59f3a98299b9829e4a96
Alex <slicer256@gmail.com>
2019-11-03 15:09:49 +0200

    reset password email template

--- app/settings.py
+++ app/settings.py
@@ -154,3 +154,4 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ['EMAIL_USER']
 EMAIL_HOST_PASSWORD = os.environ['EMAIL_PASSWORD']
 EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = "Play Tennis <info@playtennis.com.ua>"
